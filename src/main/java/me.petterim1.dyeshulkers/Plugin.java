9ae99b6969d9ba9adde4765120050c16739cc687
Petteri <petteri.malkavaara1@gmail.com>
2020-11-30 18:59:28 +0200

    Initial commit

---
+++ src/main/java/me.petterim1.dyeshulkers/Plugin.java
@@ -0,0 +1,36 @@
+package me.petterim1.dyeshulkers;
+
+import cn.nukkit.block.BlockID;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.inventory.PlayerInventory;
+import cn.nukkit.item.ItemDye;
+import cn.nukkit.plugin.PluginBase;
+
+public class Plugin extends PluginBase implements Listener {
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        if (e.getAction() == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK || e.getAction() == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
+            if (e.getItem().getId() == 351) {
+                if (e.getBlock().getId() == BlockID.SHULKER_BOX) {
+                    if (e.getPlayer().isSneaking()) {
+                        e.getBlock().level.setBlockDataAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, ((ItemDye) e.getItem()).getDyeColor().getWoolData());
+                        PlayerInventory inv = e.getPlayer().getInventory();
+                        inv.decreaseCount(inv.getHeldItemIndex());
+                    }
+                } else if (e.getBlock().getId() == BlockID.UNDYED_SHULKER_BOX) {
+                    e.getBlock().level.setBlockIdAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, BlockID.SHULKER_BOX);
+                    e.getBlock().level.setBlockDataAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, ((ItemDye) e.getItem()).getDyeColor().getWoolData());
+                    PlayerInventory inv = e.getPlayer().getInventory();
+                    inv.decreaseCount(inv.getHeldItemIndex());
+                }
+            }
+        }
+    }
+}
