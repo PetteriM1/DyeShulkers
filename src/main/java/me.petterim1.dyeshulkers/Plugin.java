d7b63e6f1f63626d739ec9bc77859930496190ac
Petteri <petteri.malkavaara1@gmail.com>
2020-12-03 15:52:35 +0200

    Fix certain new dye colors

--- src/main/java/me.petterim1.dyeshulkers/Plugin.java
+++ src/main/java/me.petterim1.dyeshulkers/Plugin.java
@@ -7,6 +7,7 @@ import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.inventory.PlayerInventory;
 import cn.nukkit.item.ItemDye;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.DyeColor;
 
 public class Plugin extends PluginBase implements Listener {
 
@@ -20,13 +21,41 @@ public class Plugin extends PluginBase implements Listener {
             if (e.getItem().getId() == 351) {
                 if (e.getBlock().getId() == BlockID.SHULKER_BOX) {
                     if (e.getPlayer().isSneaking()) {
-                        e.getBlock().level.setBlockDataAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, ((ItemDye) e.getItem()).getDyeColor().getWoolData());
+                        int color;
+                        switch (e.getItem().getDamage()) {
+                            case 19:
+                                color = DyeColor.WHITE.getWoolData();
+                                break;
+                            case 18:
+                                color = DyeColor.BLUE.getWoolData();
+                                break;
+                            case 17:
+                                color = DyeColor.BROWN.getWoolData();
+                                break;
+                            default:
+                                color = ((ItemDye) e.getItem()).getDyeColor().getWoolData();
+                        }
+                        e.getBlock().level.setBlockDataAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, color);
                         PlayerInventory inv = e.getPlayer().getInventory();
                         inv.decreaseCount(inv.getHeldItemIndex());
                     }
                 } else if (e.getBlock().getId() == BlockID.UNDYED_SHULKER_BOX) {
                     e.getBlock().level.setBlockIdAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, BlockID.SHULKER_BOX);
-                    e.getBlock().level.setBlockDataAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, ((ItemDye) e.getItem()).getDyeColor().getWoolData());
+                    int color;
+                    switch (e.getItem().getDamage()) {
+                        case 19:
+                            color = DyeColor.WHITE.getWoolData();
+                            break;
+                        case 18:
+                            color = DyeColor.BLUE.getWoolData();
+                            break;
+                        case 17:
+                            color = DyeColor.BROWN.getWoolData();
+                            break;
+                        default:
+                            color = ((ItemDye) e.getItem()).getDyeColor().getWoolData();
+                    }
+                    e.getBlock().level.setBlockDataAt((int) e.getBlock().x, (int) e.getBlock().y, (int) e.getBlock().z, color);
                     PlayerInventory inv = e.getPlayer().getInventory();
                     inv.decreaseCount(inv.getHeldItemIndex());
                 }
